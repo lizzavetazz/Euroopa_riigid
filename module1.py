f5a600d3773bd3444d0ee87cbe84503ca05c4a2b
Opilane TTHK <opilane@tthk.ee>
2022-01-25 11:36:07 +0200

    Add project files.

---
+++ module1.py
@@ -0,0 +1,6 @@
+﻿def lisa_sõna(Capitals:dict, a:str)->dict:
+	"""Lisama sõna sõnavarasse
+	"""
+	a=input("Siseta sõna tähendus >>> ") #просим ввести значение слова, мы знаем ключ из прошлой функции по поиску
+	Capitals.update({a.title():b.title()}) #добавляем в словарь
+	return Capitals
